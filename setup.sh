8e0b8982654d7566e1c50224fe4f27ae5c09731e
Matt Hauck <matthauck@gmail.com>
2015-04-25 00:02:41 -0700

    Add gitconfig

--- setup.sh
+++ setup.sh
@@ -2,11 +2,20 @@
 
 MYDIR=$(cd "$( dirname $0 )" && pwd )
 
-# add 
+# add .profile
 if [[ ! -f ~/.profile ]] || ! grep profile.sh ~/.profile 1> /dev/null 2> /dev/null; then
   #"$LOADED_PROFILE" != "true" ]; then
   echo "" >> ~/.profile
   echo "# add home-files profile" >> ~/.profile
   echo "source \"${MYDIR}/profile/profile.sh\"" >> ~/.profile
   source ~/.profile
+fi
+
+# add .gitconfig
+if [ ! -L ~/.gitconfig ]; then
+  if [ -f ~/.gitconfig ]; then 
+    echo "Moving old .gitconfig..."
+    mv ~/.gitconfig ~/.gitconfig.bak
+  fi
+  ln -s "${MYDIR}/git/gitconfig" ~/.gitconfig
 fi
